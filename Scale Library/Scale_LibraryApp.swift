1cfd546813d53e3b70ba074e2c310c8ff446727e
Tan Sheng Fong <tanshengfong@icloud.com>
2023-09-23 11:52:17 +0800

    Initial Commit

---
+++ Scale Library/Scale_LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scale_LibraryApp.swift
+//  Scale Library
+//
+//  Created by Tan Sheng Fong on 23/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Scale_LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
